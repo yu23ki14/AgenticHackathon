59d0618478ae40f0eea220a44ccdc9c99f2d20c9
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-02-05 16:22:38 +0900

    update: graph demo

--- client/app/_components/DependenciesGraph/VisualizeGraph.tsx
+++ client/app/_components/DependenciesGraph/VisualizeGraph.tsx
@@ -34,11 +34,11 @@ interface GraphEvent {
 export default function VisualizeGraph(): ReactElement {
   const graph: GraphData = React.useMemo(() => ({
     nodes: [
-      { id: 1, label: "Node 1", title: "node 1 tootip text" },
-      { id: 2, label: "Node 2", title: "node 2 tootip text" },
-      { id: 3, label: "Node 3", title: "node 3 tootip text" },
-      { id: 4, label: "Node 4", title: "node 4 tootip text" },
-      { id: 5, label: "Node 5", title: "node 5 tootip text" }
+      { id: 1, label: "Yawn-A", title: "node 1 tootip text" },
+      { id: 2, label: "Yawn-B", title: "node 2 tootip text" },
+      { id: 3, label: "Yawn-C", title: "node 3 tootip text" },
+      { id: 4, label: "Yawn-D", title: "node 4 tootip text" },
+      { id: 5, label: "Yawn-E", title: "node 5 tootip text" }
     ],
     edges: [
       { from: 1, to: 2 },
