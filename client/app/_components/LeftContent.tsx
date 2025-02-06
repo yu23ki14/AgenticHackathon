6b92f8ec3d41f296797ac6f2c24bc0c180df3e41
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-02-06 11:48:20 +0900

    feat: set up openai using vercel ai sdk

--- client/app/_components/LeftContent.tsx
+++ client/app/_components/LeftContent.tsx
@@ -2,11 +2,28 @@
 
 import * as React from "react";
 import { ReactElement } from "react";
+import { useChat } from 'ai/react';
 
 export default function LeftContent(): ReactElement {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
   return (
-    <div className="w-1/2 border-r border-gray-300 p-8">
-      LeftContent
+    <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
+      {messages.map(m => (
+        <div key={m.id} className="whitespace-pre-wrap mb-4">
+          {m.role === 'user' ? 'User: ' : 'AI: '}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed dark:bg-zinc-900 bottom-0 w-full max-w-md p-2 mb-8 border border-zinc-300 dark:border-zinc-800 rounded shadow-xl"
+          value={input}
+          placeholder="Say something..."
+          onChange={handleInputChange}
+        />
+      </form>
     </div>
   )
 }
---
+++ client/app/api/chat/route.ts
@@ -0,0 +1,29 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { openai, createOpenAI } from '@ai-sdk/openai';
+import { streamText } from 'ai';
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  
+  // const gaiaProvider = createOpenAI({
+  //   baseURL: process.env.GAIANET_API_BASE_URL ?? '',       // 例: "https://llama8b.gaia.domains/v1"
+  //   apiKey: process.env.GAIANET_API_KEY ?? '',             // Gaianetから取得したAPIキー
+  //   compatibility: 'compatible',                           // OpenAI互換モード（3rdパーティ向け設定）
+  // });
+
+  // const resultGaia = streamText({
+  //   model: gaiaProvider('llama'),  // 利用するGaianetモデルIDを指定（この例ではLlama 8bモデル）
+  //   messages,                      // ユーザからのメッセージ履歴
+  //   // 必要に応じて onFinish や他の設定も指定可能
+  // });
+
+  const result = streamText({
+    model: openai('gpt-4o'),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
