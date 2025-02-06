d7ecf49d0bae859abd5c130c163ac3efe581825a
yawn <69970183+yawn-c111@users.noreply.github.com>
2025-02-06 15:36:57 +0900

    fix: useEffect import

--- client/app/_components/LeftContent.tsx
+++ client/app/_components/LeftContent.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import * as React from "react";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 import { useDependenciesData } from "@/hooks/useDependenciesData";
 import { defaultDescriptionDataArr, defaultDistributionDataArr, defaultGraphDataArr } from "@/data/dependencies";
 import ChatBot from "./ChatBot";
@@ -9,7 +9,7 @@ import ChatBot from "./ChatBot";
 export default function LeftContent(): ReactElement {
   const { setDescriptionDataArr, setGraphDataArr, setDistributionDataArr } = useDependenciesData();
 
-  React.useEffect(() => {
+  useEffect(() => {
     setDescriptionDataArr(defaultDescriptionDataArr);
     setGraphDataArr(defaultGraphDataArr);
     setDistributionDataArr(defaultDistributionDataArr);
